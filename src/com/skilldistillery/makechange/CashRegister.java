0c09f8542dbadc8f83075556b6f50f37b1ca5674
Nick G <gonzanick@gmail.com>
2022-03-18 18:44:57 -0600

    Added var CHANGE and if loop for change. INCOMPLETE

--- src/com/skilldistillery/makechange/CashRegister.java
+++ src/com/skilldistillery/makechange/CashRegister.java
@@ -6,6 +6,7 @@ public class CashRegister {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		double change = 0.0;
 		
 		System.out.println("Please enter the price of the item(s): ");
 		double inputPrice = sc.nextDouble();
@@ -17,6 +18,11 @@ public class CashRegister {
 			System.out.println("No change needed.");
 		} else if (inputPrice > inputPay) {
 			System.out.println("Insufficient payment.");
+		}
+		
+		if (inputPrice < inputPay) {
+			change = inputPay - inputPrice;
+			
 		}
 
 	}
